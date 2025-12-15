a552bf28845535edd3e77192e56f922b6052255f
Mano Sriram <mano.sriram0@gmail.com>
2025-12-15 17:10:20 +0530

    refactor: inject mountpoints into FiberWebHandler and remove UI form

--- handlers/web/web.go
+++ handlers/web/web.go
@@ -4,14 +4,21 @@ import (
 	"path/filepath"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/manosriram/floppy/internal/config"
 )
 
-type FiberWebHandler struct{}
+type FiberWebHandler struct {
+	M config.Mountpoints
+}
 
-func NewFiberWebHandler() FiberWebHandler {
-	return FiberWebHandler{}
+func NewFiberWebHandler(M config.Mountpoints) FiberWebHandler {
+	return FiberWebHandler{
+		M: M,
+	}
 }
 
 func (f *FiberWebHandler) Home(c *fiber.Ctx) error {
+	mountPoints := f.M.ListMountPoints()
+
 	return c.SendFile(filepath.Join("web", "index.html"))
 }
